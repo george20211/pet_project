9dec692cd5b3cce5aa44dc8f51aca562e2af2b0d
george20211 <79093366+george20211@users.noreply.github.com>
2021-07-05 14:22:28 +0300

    Update settings.py

--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/2.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.2/ref/settings/
 """
-import oc
+import os.path.join(os.getcwd(), file)
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
